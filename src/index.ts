4bf27ab560f527fe7c34f2bf871bdb014a56a82a
TriptoAfsin <AfsinTripto@gmail.com>
2024-02-26 15:52:41 +0600

    update: add support for timeBang, dayBang, currencyToBang

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import currencyToBang from "./utils/currencyToBang";
 import dateBang from "./utils/dateBang";
+import dayBang from "./utils/dayBang";
 import monthBang from "./utils/monthBang";
 import numBang from "./utils/numBang";
 import numToBanglaWords from "./utils/numToBanglaWords";
+import timeBang from "./utils/timeBang";
 
-export { dateBang, monthBang, numBang, numToBanglaWords };
+export {
+  currencyToBang,
+  dateBang,
+  dayBang,
+  monthBang,
+  numBang,
+  numToBanglaWords,
+  timeBang,
+};
