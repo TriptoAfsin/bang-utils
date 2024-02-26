b40eea9e84dfb455086e6dc096dc383111b911a1
TriptoAfsin <AfsinTripto@gmail.com>
2024-02-26 13:52:36 +0600

    chore: update root file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import dateBang from "./utils/dateBang";
+import monthBang from "./utils/monthBang";
 import numBang from "./utils/numBang";
+import numToBanglaWords from "./utils/numToBanglaWords";
 
-export { dateBang, numBang };
+export { dateBang, monthBang, numBang, numToBanglaWords };
