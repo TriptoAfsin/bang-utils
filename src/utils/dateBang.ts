dc53440be71914404e5657d122e5ffc8f1f9019a
TriptoAfsin <AfsinTripto@gmail.com>
2024-02-26 13:51:55 +0600

    chore: recfactor dateBang

--- src/utils/dateBang.ts
+++ src/utils/dateBang.ts
@@ -1,27 +1,61 @@
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import monthBang from "./monthBang";
 import numBang from "./numBang";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+type DateFormatType =
+  | "D/M/YY"
+  | "M/D/YY"
+  | "D/M/YYYY"
+  | "M/D/YYYY"
+  | "D M, YYYY"
+  | "D M, YY";
+
+const getFormatted = (
+  format: DateFormatType,
+  separator: string,
+  date: string
+) => {
+  const year: any = dayjs(date).get("year");
+  const sYear: any = dayjs(date).get("y");
+  const month: any = dayjs(date).get("month");
+  const day: any = dayjs(date).tz("Asia/Dhaka").get("date");
+  const mapping: any = {
+    "D/M/YY": `${numBang(String(parseInt(day)))}${separator}${numBang(
+      String(parseInt(month) + 1)
+    )}${separator}${numBang(String(sYear).slice(-2))}`,
+    "D/M/YYYY": `${numBang(String(parseInt(day)))}${separator}${numBang(
+      String(parseInt(month) + 1)
+    )}${separator}${numBang(String(year))}`,
+    "M/D/YY": `${numBang(String(parseInt(month) + 1))}${separator}${numBang(
+      String(parseInt(day))
+    )}${separator}${numBang(String(sYear).slice(-2))}`,
+    "M/D/YYYY": `${numBang(String(parseInt(month) + 1))}${separator}${numBang(
+      String(parseInt(day))
+    )}${separator}${numBang(String(year))}`,
+    "D M, YYYY": `${numBang(String(parseInt(day)))} ${monthBang(
+      date
+    )}${separator}${numBang(String(year))}`,
+    "D M, YY": `${numBang(String(parseInt(day)))} ${monthBang(
+      date
+    )}${separator}${numBang(String(year).slice(-2))}`,
+  };
+
+  return mapping[format];
+};
+
 const dateBang = (
   engDate: string = "2023-07-03",
-  separator: string = "/"
+  separator: string = "/",
+  format: DateFormatType = "D/M/YY"
 ): string | void => {
   try {
     if (dayjs(engDate).isValid()) {
-      const engYear: any = dayjs(engDate).get("year");
-      const engMonth: any = dayjs(engDate).get("month");
-      const engDay: any = dayjs(engDate).tz("Asia/Dhaka").get("date");
-      // console.log('engDay', engDay)
-      const bdDate: string = `${numBang(
-        String(parseInt(engDay))
-      )}${separator}${numBang(
-        String(parseInt(engMonth) + 1)
-      )}${separator}${numBang(String(engYear))}`;
-
+      const bdDate: string = getFormatted(format, separator, engDate);
       return bdDate;
     } else {
       console.error("Invalid date");
