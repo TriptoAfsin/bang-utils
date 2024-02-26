791ab79570cbf2c0e54a3ed4e2052f9e1f0c932e
TriptoAfsin <AfsinTripto@gmail.com>
2024-02-26 13:50:57 +0600

    doc: update example app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-const { avg, corelation, extrema, max } = require("../dist/index");
+//examples
 
-console.log(avg([1, 2, 3, 4, 5]));
+const bangUtils = require("../dist/index");
+
+const { dateBang, numBang, monthBang, numToBanglaWords } = bangUtils;
+
+console.log("number: ", numBang(21)); //number: ২১
+console.log("decimal number: ", numBang(21.21)); //decimal number: ২১.২১
+
+console.log("month: ", monthBang("2024-05-02")); //month: মে
+
+console.log("date: ", dateBang("2-5-2024", ",", "D M, YYYY")); //date: ৫ ফেব্রুয়ারি, ২০২৪
+console.log("to Bangla  words: ", numToBanglaWords(50.25)); //to Bangla  words: পঞ্চাশ দশমিক দুই পাঁচ
